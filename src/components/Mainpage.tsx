c9e000befd9a82d7654615c4efb69309ef024765
Elgcha <vkdlfl681@gmail.com>
2023-09-19 21:42:16 +0900

    bug fix: onchange usestate delay

--- src/components/Mainpage.tsx
+++ src/components/Mainpage.tsx
@@ -31,7 +31,9 @@ export default function MainText() {
         value={value}
         onChange={(e) => {
           setValue(e.target.value);
-          save = btoa(unescape(encodeURIComponent(JSON.stringify(value))));
+          save = btoa(
+            unescape(encodeURIComponent(JSON.stringify(e.target.value)))
+          );
           window.history.pushState({}, "", "?t=" + save);
         }}
         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
